022b06b5bcbed94a86d623a3d5a8253e13d6b654
feelings-for-peperoncino <87107270+feelings-for-peperoncino@users.noreply.github.com>
2023-09-20 17:45:12 +0900

    更新
    計算ボタンを廃止し、数字を入れた時点でリアルタイムに計算するようにしました

--- js/script.js
+++ js/script.js
@@ -1,68 +1,27 @@
-const priceElements = document.querySelectorAll('.price');
-const quantityInputs = document.querySelectorAll('.quantity-input');
-
-priceElements.forEach((priceElement, index) => {
-  priceElement.addEventListener('click', () => {
-    updateTotal(index);
-  });
-});
-
-quantityInputs.forEach((quantityInput, index) => {
-  quantityInput.addEventListener('click', () => {
-    if (quantityInput.value === '0') {
-      quantityInput.value = '';
-    }
-  });
-
-  quantityInput.addEventListener('input', () => {
-    updateTotal(index);
-  });
-});
-
-function updateTotal(index) {
-  const priceElement = priceElements[index];
-  const quantityElement = quantityInputs[index];
-  const totalElement = priceElement.parentElement.querySelector('.total');
-  const totalQuantityElement = document.querySelector('.total-quantity');
-  const grandTotalElement = document.querySelector('.grand-total');
-
-  const priceValue = parseFloat(priceElement.getAttribute('data-value'));
-  const quantity = parseInt(quantityElement.value) || 0;
-  const total = priceValue * quantity;
-
-  totalElement.textContent = total === Math.floor(total) ? total + '円' : total.toFixed(2) + '円';
-
-  let totalQuantity = 0;
-  quantityInputs.forEach((input) => {
-    totalQuantity += parseInt(input.value) || 0;
-  });
-  totalQuantityElement.textContent = totalQuantity;
-
-  let grandTotal = 0;
-  priceElements.forEach((element, i) => {
-    grandTotal += parseFloat(element.parentElement.querySelector('.total').textContent);
-  });
-
-  grandTotalElement.textContent = grandTotal === Math.floor(grandTotal) ? grandTotal + '円' : grandTotal.toFixed(2) + '円';
-}
-
-
-
-// 計算ボタンを押した時
-// 割引計算ボタンをクリックしたときの処理
-// 割引計算ボタンをクリックしたときの処理
 document.addEventListener("DOMContentLoaded", function () {
-  const calculateButton = document.getElementById("calculateButton");
   const discountInput = document.getElementById("discountInput");
   const priceElements = document.querySelectorAll(".price");
   const originalPrices = Array.from(priceElements).map((priceElement) => parseFloat(priceElement.getAttribute("data-value")));
   const quantityInputs = document.querySelectorAll('.quantity-input');
   const totalElements = document.querySelectorAll('.total'); // 各ゆうパックの合計金額要素
 
-  calculateButton.addEventListener("click", calculateDiscount);
+  discountInput.addEventListener("input", calculateDiscount);
+
+  // 個数を入力するボタンにもリアルタイムで反映するためのイベントリスナーを追加
+  quantityInputs.forEach((quantityInput, index) => {
+    quantityInput.addEventListener("input", () => {
+      updateTotal(index);
+      calculateDiscount(); // 割引もリアルタイムで更新
+    });
+  });
 
   function calculateDiscount() {
     const discountRate = parseInt(discountInput.value, 10);
+
+    // 割引率が入力されているかチェック
+    if (isNaN(discountRate)) {
+      return; // 割引率が空の場合、何もしない
+    }
 
     if (discountRate >= 0 && discountRate <= 99) {
       // 合計金額を初期化
@@ -99,7 +58,36 @@ document.addEventListener("DOMContentLoaded", function () {
       const grandTotalElement = document.querySelector('.grand-total');
       grandTotalElement.textContent = grandTotal === Math.floor(grandTotal) ? grandTotal + '円' : grandTotal.toFixed(2) + '円';
     } else {
+      // 割引率が範囲外の場合、エラーメッセージを表示
       alert("割引率は0から99の間で入力してください。");
     }
+  }
+
+  function updateTotal(index) {
+    // 個数を入力したときの処理をそのまま移動
+    const priceElement = priceElements[index];
+    const quantityElement = quantityInputs[index];
+    const totalElement = priceElement.parentElement.querySelector('.total');
+    const totalQuantityElement = document.querySelector('.total-quantity');
+    const grandTotalElement = document.querySelector('.grand-total');
+
+    const priceValue = parseFloat(priceElement.getAttribute('data-value'));
+    const quantity = parseInt(quantityElement.value) || 0;
+    const total = priceValue * quantity;
+
+    totalElement.textContent = total === Math.floor(total) ? total + '円' : total.toFixed(2) + '円';
+
+    let totalQuantity = 0;
+    quantityInputs.forEach((input) => {
+      totalQuantity += parseInt(input.value) || 0;
+    });
+    totalQuantityElement.textContent = totalQuantity;
+
+    let grandTotal = 0;
+    priceElements.forEach((element, i) => {
+      grandTotal += parseFloat(element.parentElement.querySelector('.total').textContent);
+    });
+
+    grandTotalElement.textContent = grandTotal === Math.floor(grandTotal) ? grandTotal + '円' : grandTotal.toFixed(2) + '円';
   }
 });
